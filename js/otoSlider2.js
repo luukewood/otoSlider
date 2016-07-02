7d5e85ba1e82f08afca5efd1d29f9cf4eede9d2b
Lukasz Dziedziola <dziedziola.lukasz@gmail.com>
2016-07-02 18:50:31 +0200

    Changed js files

--- js/otoSlider2.js
+++ js/otoSlider2.js
@@ -1,61 +1,53 @@
 (function() {
 
 
-var sliderContainer = document.querySelector('.slides');	
-var	sliderItems = sliderContainer.children;
-var itemsLength = sliderItems.length;
-var widthImage = sliderItems[0].children[0].offsetWidth;
-var sliderWidth = widthImage * itemsLength;
-var next = document.querySelector("#next");
-var prev = document.querySelector("#prev");
-var clonedElement;
-var firstElement;
+	var sliderContainer = document.querySelector('.slides');	
+	var	sliderItems = sliderContainer.children;
+	var itemsLength = sliderItems.length;
+	var widthImage = sliderItems[0].children[0].offsetWidth;
+	var sliderWidth = widthImage * itemsLength;
+	var next = document.querySelector("#next");
+	var prev = document.querySelector("#prev");
+	var clonedElement;
+	var firstElement;
 
-sliderContainer.style.width = sliderWidth + "px";
-
-current = 0;
-
-
-
-
+	sliderContainer.style.width = sliderWidth + "px";
 
 	sliderContainer.style.left = -100 + "%";
 	sliderContainer.classList.add("horizTranslate");
 	var makeSlide = setInterval(animation, 8000);
 
  
-function animation() {
+	function animation() {
 
-	sliderContainer.style.left = parseInt(sliderContainer.style.left) + (-100) + "%";
-	sliderContainer.addEventListener("transitionend",resetLeft, false);
-	sliderContainer.classList.add("horizTranslate");
+		sliderContainer.style.left = parseInt(sliderContainer.style.left) + (-100) + "%";
+		sliderContainer.addEventListener("transitionend",resetLeft, false);
+		sliderContainer.classList.add("horizTranslate");
 
-};
+	};
 
 
-function resetLeft() {
+	function resetLeft() {
 
-	// var newElement = sliderItems[current].cloneNode(true);
+		// var newElement = sliderItems[current].cloneNode(true);
 
-	firstElement = sliderContainer.firstElementChild;
-	console.log(firstElement);
+		firstElement = sliderContainer.firstElementChild;
+		console.log(firstElement);
+		clonedElement = firstElement.cloneNode(true);
+		firstElement.remove();
+		sliderContainer.appendChild(clonedElement);
+		sliderContainer.style.left = -100 + "%";
+		sliderContainer.classList.remove("horizTranslate");
 
-	clonedElement = firstElement.cloneNode(true);
+		// console.log(sliderItems[0]);
+		// var removed = sliderItems[0].remove().cloneNode(true);
+		// var removed = sliderItems[0].remove();
+		
+		// sliderItems.appendChild(removed);
 
-	firstElement.remove();
-
-	sliderContainer.appendChild(clonedElement);
-
-	// console.log(sliderItems[0]);
-	// var removed = sliderItems[0].remove().cloneNode(true);
-	// var removed = sliderItems[0].remove();
-	sliderContainer.style.left = -100 + "%";
-	sliderContainer.classList.remove("horizTranslate");
-	// sliderItems.appendChild(removed);
-
-	// console.log("Działa reset");
-	// current++;
-}
-	
+		// console.log("Działa reset");
+		// current++;
+	}
+		
 
 })();
